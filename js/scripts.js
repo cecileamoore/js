c48f796e305a0cfe304f29a0143091ed4cda6e01
Cecile Moore <cecileamoore@gmail.com>
2015-11-03 16:09:00 -0500

    final

---
+++ js/scripts.js
@@ -0,0 +1,51 @@
+// Create a “cheat sheet” JavaScript file that contains examples of all of the things we learned today, delineated with comments:
+
+
+// Variables
+	//In JavaScript, a variable is simply a container for a value
+		//var name = ""
+		//var numberOfCars = ""
+
+// Debugging (alerts, comments, the console)
+
+	//comments: 
+		// for single line
+
+		/*for
+		multiple
+		lines*/
+
+	//console: built-in tood in browser to fun JavaScript code directly
+	//alerts: 'pop-ups', output appears as soon as the alert () function is called
+		//alert can be in console or in a scirpt (a .js file)
+
+// Different data types
+	
+	//String - "Hello World"
+	//Number - 5,5.5,1000(all numbers in JS are floats)
+	//Boolean - true, false
+	//Underfined (no value)
+
+// Arrays
+	//used to hold a collection of data, any data type
+		//ex: ["Moe", "Larry", "Curly"]
+		//can also combine data types: [1,3,5,7,"female", "male"]
+
+// Testing
+	//use this to test:
+		/* function myFunction (){
+			var obj = document.getElementById("h01");
+			obj.innerHTML = "Hello jQuery";
+		}
+		onload = myFunction;
+
+// Logic
+	//logical operators used to determine logic between variables/values
+		//Logical AND (&&)
+		//Logical OR (||)
+		//Logical NOTE (!)
+
+// Functions
+	//block of code used to preform a task, executed when invokes
+
+// Write 3 different functions that take input and return something via the console or an alert 
